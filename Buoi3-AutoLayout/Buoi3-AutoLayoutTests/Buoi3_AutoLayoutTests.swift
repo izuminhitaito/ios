96f32fb9e0b48322383927fae9b58d3dbbc8d57c
izumin <izumin.hitaito@gmail.com>
2018-03-09 17:05:25 +0700

    bat dau project demo autolayout khoapham

---
+++ Buoi3-AutoLayout/Buoi3-AutoLayoutTests/Buoi3_AutoLayoutTests.swift
@@ -0,0 +1,36 @@
+//
+//  Buoi3_AutoLayoutTests.swift
+//  Buoi3-AutoLayoutTests
+//
+//  Created by ivc on 3/9/18.
+//  Copyright © 2018 ivc. All rights reserved.
+//
+
+import XCTest
+@testable import Buoi3_AutoLayout
+
+class Buoi3_AutoLayoutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
